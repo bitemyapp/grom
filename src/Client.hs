ee859fd39edc5fb92d25a9256897f0f36b181aaa
Chris Allen <cma@bitemyapp.com>
2013-12-12 22:10:03 -0800

    bitta progress :)

--- src/Client.hs
+++ src/Client.hs
@@ -1,9 +1,19 @@
 module Client where
 
 import qualified Data.BEncode as BE
-import qualified Data.Map as Map
+import Data.ByteString.Lazy.Char8 as BS
+import qualified Data.Map.Strict as Map
 import Network.Socket
 
 -- Map.fromList [("code", "(+ 1 2)"), ("id", "lolsorandom"), ("op", "eval")]
 -- (BE.bShow $ BE.BList [(BE.BInt 10), (BE.BInt 20)]) ""
--- (BE.bShow $ BE.BString $ Data.ByteString.Lazy.Char8.pack "blah") ""
+-- (BE.bShow $ BE.BString $ BS.pack "blah") ""
+-- (BE.bShow $ BE.BDict $ Map.fromList [("code", "(+ 1 2)"), ("id", "lolsorandom"), ("op", "eval")]
+
+bstringFromString s = BE.BString $ BS.pack s
+bencodeMap m = fmap bstringFromString m
+
+-- Map.fromList [("code", "(+ 1 2)"), ("id", "lolsorandom"), ("op", "eval")]
+-- d2:id11:lolsorandom2:ns4:user7:session36:f70b7346-94ae-401c-9ffe-4fbda16077fd5:value1:3ed2:id11:lolsorandom7:session36:f70b7346-94ae-401c-9ffe-4fbda16077fd6:statusl4:doneee
+
+-- 10blahwootAA
